6d8bf0097b78b03140126c87c00da3ee1fc3e868
rafaelfb344355 <107368915+rafaelfb344355@users.noreply.github.com>
2024-03-20 17:35:02 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
-import { Console } from "console";
-import fastify from "fastify";
+
+import fastify from "fastify"
+import { z } from "zod"
  
 const app = fastify()
+
+app.post(`/links`, (request) =>{
+
+   const createLinkSchema =z.object({
+code: z.string().min(3),
+url: z.string().url(),
+    })
+const{code,url} =createLinkSchema.parse(request.body)
+    
+//1:17
+})
 
 app.get('/test', () =>{
     return 'hii'
