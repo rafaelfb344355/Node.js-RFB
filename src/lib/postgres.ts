f4d981e26e917764298ad3dbb1fda20082801a7b
rafaelfb344355 <rafael344355@gmail.com>
2024-03-21 12:08:10 -0300

    On branch main  Changes to be committed: 	modified:   .gitignore 	deleted:    README.md 	modified:   client.http 	modified:   docker-compose.yml 	modified:   package-lock.json 	modified:   package.json 	modified:   src/lib/postgres.ts 	modified:   src/lib/redis.ts 	modified:   src/server.ts 	new file:   src/setup.ts 	deleted:    src/stup.ts 	modified:   tsconfig.json

--- src/lib/postgres.ts
+++ src/lib/postgres.ts
@@ -1,3 +1,3 @@
-import postgres from "postgres";
+import postgres from 'postgres'
 
-export const sql = postgres('postgresql://docker:docker@localhost5432/shortlinks')
+export const sql = postgres('postgresql://docker:docker@localhost:5432/shortlinks')
